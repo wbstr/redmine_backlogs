deaa9654d03eb84ea04e917ea47a009059a7868b
Tamas Futo <futo.tamas@webstar.hu>
2016-10-07 17:15:18 +0200

    #30500 validation when story follows task status

--- lib/backlogs_story_status_update_patch.rb
+++ lib/backlogs_story_status_update_patch.rb
@@ -20,22 +20,19 @@ module Backlogs
     module InstanceMethods
 
       def update_parent_story_status_by_rules
-        Rails.logger.info("***** OnSave #{self}")
+        Rails.logger.debug("*** OnSave #{self}")
 
         parent = self.parent
 
         return if parent.nil?
         return unless self.is_task?
         return unless parent.is_story?
-        return unless Backlogs.configured?(project) && Backlogs.setting[:story_follow_task_status] == 'nil'
+        return unless Backlogs.configured?(project)
+        return unless Backlogs.setting[:story_follow_task_status] == 'nil' # to prevent conflict between backlog's status follow feature and this one
+        return unless User.current.allowed_to?(:update_stories, parent.project)
 
         rules = Backlogs.setting[:story_status_update_rules]
         rules = RbStoryStatusUpdateRule.rules_from_json(rules)
-
-        # :original_story_statuses
-        # :new_task_status
-        # :check_all_task
-        # :new_story_status
 
         first_matching_rule = rules.
             select{|rule| rule.new_task_status == self.status.id}.
@@ -43,12 +40,17 @@ module Backlogs
             select{|rule| !rule.check_all_task || parent.children.all?{|child| rule.new_task_status == child.status.id}}.
             first
 
-        Rails.logger.info("*** Matching rule:#{first_matching_rule}")
+        Rails.logger.debug("*** Matching story status update rule: #{first_matching_rule}")
 
         if first_matching_rule
-          # egyéb vizsgálatok? jogosultság? lezártság?
           new_story_status_id = first_matching_rule.new_story_status
-          Rails.logger.info("*** Updating #{parent.id}'s status from #{parent.status.id} to #{new_story_status_id}")
+          allowed_new_status_ids = parent.new_statuses_allowed_to().map{|s| s.id}
+
+          Rails.logger.debug("*** Allowed new statuses for story: #{allowed_new_status_ids}")
+
+          return unless allowed_new_status_ids.include?(new_story_status_id)
+
+          Rails.logger.debug("*** Updating #{parent.id}'s status from #{parent.status.id} to #{new_story_status_id}")
           parent.journalized_update_attribute(:status_id, new_story_status_id)
         end
       end
